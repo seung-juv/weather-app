150953e320a2cf94d357f102245ad2a0acb411bb
seungju <kingsky32@gmail.com>
2020-08-02 04:31:58 +0900

    Asking for Permissions

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Loader from "./Loader";
 import * as Location from "expo-location";
+import { Alert } from "react-native";
 
 export default () => {
+  const [isLoading, setIsLoading] = useState(true);
   const getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      const { status } = await Location.requestPermissionsAsync();
+      if (status) {
+        const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+        setIsLoading(false);
+      } else {
+        Alert.alert("We need location permission");
+      }
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
   };
   getLocation();
-  return <Loader />;
+  return isLoading ? <Loader /> : null;
 };
