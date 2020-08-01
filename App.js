d6d739088aab1c204ce9072052a7c31a88b2b9e0
seungju <kingsky32@gmail.com>
2020-08-02 04:50:13 +0900

    Getting the Weather

--- App.js
+++ App.js
@@ -2,15 +2,25 @@ import React, { useState } from "react";
 import Loader from "./Loader";
 import * as Location from "expo-location";
 import { Alert } from "react-native";
+import axios from "axios";
+
+const API_KEY = "1d9b1f306da5bf7299871338be75b327";
 
 export default () => {
   const [isLoading, setIsLoading] = useState(true);
+  const getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
+  };
   const getLocation = async () => {
     try {
       const { status } = await Location.requestPermissionsAsync();
       if (status) {
         const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
         setIsLoading(false);
+        getWeather(latitude, longitude);
       } else {
         Alert.alert("We need location permission");
       }
